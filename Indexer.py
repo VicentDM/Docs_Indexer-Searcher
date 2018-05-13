dc7196aac8f3bb9417823d9a93feaf5b7356beca
Vicentico22 <vdm97@hotmail.com>
2018-05-13 05:34:47 +0200

    Add files via upload

---
+++ Indexer.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import os
+
+from whoosh.index import create_in
+from whoosh.fields import Schema, ID, TEXT
+
+schema = Schema(  #shema for index
+    title=TEXT(stored=True),
+    idGlobal=ID(stored=True),
+    idLocal=ID(stored=True),
+    content=TEXT)
+idir = "index_dir"
+dir = 'january'
+dict = "dictIndex.dat"
+
+f = open(dict, 'w')
+countGlobal = 0
+fileList = [f.name for f in os.scandir(dir) if f.is_file()]
+
+if not os.path.exists(idir):
+	os.mkdir(idir)
+
+ix = create_in(idir, schema)
+writer = ix.writer()
+dictIndex = {}
+
+for i in fileList:  #file list loop
+	r = dir + '/' + str(i)
+	fi = open(r, 'r')
+	text = fi.read()
+	splitText = text.split('<DOC>')
+	countLocal = 0
+	
+	for j in splitText:
+		writer.add_document(
+		    title=i,
+		    idGlobal=str(countGlobal),
+		    idLocal=str(countLocal),
+		    content=j)
+		dictIndex[countGlobal] = j
+		countGlobal += 1
+		countLocal += 1
+writer.commit()
+f.write(str(dictIndex))
+f.close()
+print('Indexing done.')
---
+++ Searcher.py
@@ -0,0 +1,37 @@
+import ast
+import re
+from whoosh.index import open_dir
+from whoosh.qparser import QueryParser
+
+# use ANDNOT to combine a word with a not one: valencia ANDNOT salenko
+# use AND to combine search: "Los Angeles" AND Aeroflot
+def cleanhtml(raw):
+	cleanr = re.compile('<.*?>')
+	cleantext = re.sub(cleanr, '', raw)
+	return cleantext
+
+
+fi2 = open('dictIndex.dat', 'r')
+dic = ast.literal_eval(fi2.read())
+ix = open_dir("index_dir")
+
+with ix.searcher() as searcher:
+	while True:
+		text = input("\n\nsearcher:")
+		if len(text) == 0:
+			break
+		query = QueryParser("content", ix.schema).parse(text)
+		results = searcher.search(query, limit=None)
+		#        print(dir(results))
+		#        print(results.docs)
+		idList = []
+		for r in results:
+			key = r['idGlobal']
+			idList.append(cleanhtml(dic[int(key)]))
+			print('file: '+r['title']+' \ndoc global ID: '+r['idGlobal']
+			      +' \ndoc local ID: '+r['idLocal']+'\n\n')
+
+		if len(results) < 4:
+			for l in idList:
+				print('\n\n')
+				print(str(l))
